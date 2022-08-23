5faa174e0bac260c5642c4ef8d9ab15da5ecc3cc
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-23 17:36:59 +0200

    .

---
+++ T26-Spring-Mysql-Ex4/src/main/java/com/juan/ex4/T26SpringMysqlEx4Application.java
@@ -0,0 +1,13 @@
+package com.juan.ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26SpringMysqlEx4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26SpringMysqlEx4Application.class, args);
+	}
+
+}
---
+++ T26-Spring-Mysql-Ex4/src/test/java/com/juan/ex4/T26SpringMysqlEx4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T26SpringMysqlEx4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
